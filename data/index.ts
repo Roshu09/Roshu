9649994bf6d365538bd02ed480b6035bb837ce48
Roshan Gupta <roshangupta7479@gmail.com>
2024-05-20 02:21:24 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,221 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently learning a JS Animation library",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "3D Solar System Planets to Explore",
+      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+      img: "/p1.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "https://github.com/Roshu09/Nishify",
+    },
+    {
+      id: 2,
+      title: "ExploreX a tour & Travel guide App",
+      des: "Simplify your Tracking in the dane forest and mountains.with Seamless experience and offline connectivit with friends.",
+      img: "/explorex.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://github.com/Roshu09/ExploreX",
+    },
+    {
+      id: 3,
+      title: "AI Image SaaS - Canva Application",
+      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+      img: "/brainwave.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "https://github.com/Roshu09/Brainwave",
+    },
+    {
+      id: 4,
+      title: "AI-article summarize ",     
+      des: "Recreated this with the summarize extractor API",
+      img: "/easeai.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/Roshu09/EaseAI",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Roshu's web skills are funnier than my best punchlines! He didn't just upgrade my website; he turned it into a comedy goldmine. His creativity is like my jokes – top-notch and totally meme-worthy. If you want your project to be the life of the online party, Roshu's your hilarious web wizard!.",
+      name: "Tanmay Bhatt",
+      title: "Content Creator?",
+    },
+   
+    {
+      quote:
+        "Roshu rocks! He didn't just upgrade my website; he made it so cool, even Spidey's jealous. His skills are as sharp as my katanas, and his dedication is like a superhero on caffeine. Hire him before I decide to crash your project, and trust me, you don't want that.",
+      name: "Deadpool",
+      title: "Ex X-MEN",
+    },
+    {
+      quote:
+        "Working with Roshu was a rare potion of efficiency and brilliance. He didn't just assist in updating Hogwarts' archaic systems; he managed to keep Peeves at bay and even taught the Marauder's Map some new tricks.If you need results that are nothing short of magical, Roshu is your wizard.",
+      name: "Severus Snape",
+      title: "Hogwarts' Professor ",
+    },
+    {
+      quote:
+        "Collaborating with Roshu was like launching a new rocket: thrilling and groundbreaking. He assisted me in developing the next generation of Tesla's AI and even brainstormed ideas for colonizing Mars. His innovative mindset, technical prowess, and relentless dedication make him a true asset to any ambitious project. If you're looking to reach for the stars,.",
+      name: "Elon Mask",
+      title: "Director of SpaceX Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Roshu was an absolute pleasure.He assisted me to develop my next AI to feel my virtual presence after the Endgame.He also working to advance up my suit.  His professionalism, promptness, and dedication to delivering results had me wondering if he's secretly part of the Avengers. Seriously, if you need someone to save your project (or the world), Roshu's your guy.",
+      name: "Tony Stark",
+      title: "Iron Man",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "github",
+      img: "/s.svg",
+      nameImg: "/github.png",
+    },
+    {
+      id: 5,
+      name: "MongoDB.",
+      img: "/react.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Devlopment Intern",
+      desc: "Assisted in the development of a web-based platform in a team using React.js, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Web Dev - Central Coalfields Limited, Ranchi",
+      desc: "Designed and developed Website individually using Bootstrap, js, Mysql, Php.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance App Dev Project",
+      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Lead Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/Roshu09",
+    },
+    {
+      id: 2,
+      img: "/instagram.svg",
+      link: "https://www.instagram.com/g.h.o.s.t80",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link: "https://www.linkedin.com/in/roshan-gupta-340887227",
+    },
+  ];
